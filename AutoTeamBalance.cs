4b703d3f735bd035b727d80d96041fb151fdd24a
Aw3K <dedkam1@wp.pl>
2024-02-27 22:30:49 +0100

    Balance happens after round ends, clears pending every round

--- AutoTeamBalance.cs
+++ AutoTeamBalance.cs
@@ -1,7 +1,9 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Utils;
+using System.Numerics;
 
 namespace AutoTeamBalance;
 
@@ -9,25 +11,56 @@ public class AutoTeamBalance : BasePlugin
 {
     public override string ModuleName => "AutoTeamBalance";
     public override string ModuleAuthor => "NyggaBytes";
-    public override string ModuleVersion => "1.0.3";
+    public override string ModuleVersion => "1.0.4";
 
-    public int[] TeamCount = new int[4];
+    private int[] TeamCount = new int[4];
+    private List<CCSPlayerController> balancePending = new List<CCSPlayerController>();
     public override void Load(bool hotReload)
     {
         Array.Clear(TeamCount, 0, 4);
+        balancePending.Clear();
     }
 
     #region Events
 
     [GameEventHandler(HookMode.Pre)]
+    public HookResult OnRoundPreStart(EventRoundEnd @event, GameEventInfo info) {
+        foreach (var kvp in balancePending) {
+            var player = Utilities.GetPlayerFromSteamId(kvp.SteamID)!;
+            if (!player.IsHLTV
+            && player != null
+            && !player.IsBot
+            && player.IsValid
+            && player.Connected == PlayerConnectedState.PlayerConnected ){
+                int[] TeamCountBefore = (int[])TeamCount.Clone();
+                if (Math.Abs(TeamCount[2] - TeamCount[3]) >= 2) {
+                    if (TeamCount[2] > TeamCount[3])
+                    {
+                        if (player.Team == CsTeam.Terrorist) { player.SwitchTeam(CsTeam.CounterTerrorist); }
+                        player.PrintToChat(Localizer["ForcedChangedTeam"]);
+                        Server.PrintToConsole("[AutoTeamBalance]: Moved " + player.PlayerName + " [" + player.SteamID + "] to the team: " + player.Team.ToString() + " (Before[tt:" + TeamCountBefore[2] + " ct:" + TeamCountBefore[3] + "] After[tt:" + TeamCount[2] + " ct:" + TeamCount[3] + "])");
+                    }
+                    else if (TeamCount[3] > TeamCount[2])
+                    {
+                        if (player.Team == CsTeam.CounterTerrorist) { player.SwitchTeam(CsTeam.Terrorist); }
+                        player.PrintToChat(Localizer["ForcedChangedTeam"]);
+                        Server.PrintToConsole("[AutoTeamBalance]: Moved " + player.PlayerName + " [" + player.SteamID + "] to the team: " + player.Team.ToString() + " (Before[tt:" + TeamCountBefore[2] + " ct:" + TeamCountBefore[3] + "] After[tt:" + TeamCount[2] + " ct:" + TeamCount[3] + "])");
+                    }
+                }
+            }
+        }
+        balancePending.Clear();
+        return HookResult.Continue;
+    }
+    [GameEventHandler(HookMode.Pre)]
     public HookResult OnPlayerTeam(EventPlayerTeam @event, GameEventInfo info)
     {
         CCSPlayerController? player = @event.Userid;
-
         if (player.IsHLTV
-         || player == null
-         || player.IsBot
-         || !player.IsValid
+        || player == null
+        || player.IsBot
+        || !player.IsValid
+        || player.Connected != PlayerConnectedState.PlayerConnected
         ) return HookResult.Continue;
 
         int newTeam = @event.Team;
@@ -81,17 +114,8 @@ public class AutoTeamBalance : BasePlugin
 
         if (Math.Abs(TeamCount[2] - TeamCount[3]) >= 2)
         {
-            int[] TeamCountBefore = (int[])TeamCount.Clone();
-            if (TeamCount[2] > TeamCount[3]) {
-                if (player.Team == CsTeam.Terrorist) { player.SwitchTeam(CsTeam.CounterTerrorist); }
-                player.PrintToChat(Localizer["ForcedChangedTeam"]);
-                Server.PrintToConsole("[AutoTeamBalance]: Moved " + player.PlayerName + " [" + player.SteamID + "] to the team: " + player.Team.ToString() + " (Before[tt:" + TeamCountBefore[2] + " ct:" + TeamCountBefore[3] + "] After[tt:" + TeamCount[2] + " ct:" + TeamCount[3] + "])");
-            }
-            else if (TeamCount[3] > TeamCount[2]) {
-                if (player.Team == CsTeam.CounterTerrorist) { player.SwitchTeam(CsTeam.Terrorist); }
-                player.PrintToChat(Localizer["ForcedChangedTeam"]);
-                Server.PrintToConsole("[AutoTeamBalance]: Moved " + player.PlayerName + " [" + player.SteamID + "] to the team: " + player.Team.ToString() + " (Before[tt:" + TeamCountBefore[2] + " ct:" + TeamCountBefore[3] + "] After[tt:" + TeamCount[2] + " ct:" + TeamCount[3] + "])");
-            }
+            if (TeamCount[2] > TeamCount[3]) { balancePending.Add(player); }
+            else if (TeamCount[3] > TeamCount[2]) { balancePending.Add(player); }
         }
         return HookResult.Continue;
     }
